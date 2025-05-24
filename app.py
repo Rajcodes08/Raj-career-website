b06f8ea1d3a3b5e52f077465882dc4f3b921e3d6
Raj <aprajkishore@gmail.com>
2025-05-24 17:31:29 +0530

    done rendering using jinja templates

--- app.py
+++ app.py
@@ -3,11 +3,37 @@ from flask import Flask, render_template # flask is module # Flask is a class
 app = Flask(__name__) # __name__ is a special variable in Python that holds the name of the current module # app is object of class Flask
 # __name__ is used to determine if the script is being run directly or imported as a module
 
+JOBS = [
+    {
+        'id': 1,
+        'title': 'Data Analyst',
+        'location': 'Bengaluru, India',
+        'salary': 'Rs. 10,00,000'
+    },
+    {
+        'id': 2,
+        'title': 'Data Scientist',
+        'location': 'Delhi, India',
+        'salary': 'Rs. 15,00,000'
+    },
+    {
+        'id': 3,
+        'title': 'Frontend Engineer',
+        'location': 'Remote'
+    },
+    {
+        'id': 4,
+        'title': 'Backend Engineer',
+        'location': 'San Francisco, USA',
+        'salary': 'Rs. 25,00,000'
+    }
+]
+
 @app.route('/') # jovian.ai/learn here jovian.ai is the domain and /learn is the path or route
  # / is empty path or root path
 # @app.route('/hello') # this is a decorator that tells Flask what URL should call the function that follows
 def hello_world():
-    return render_template('home.html') # this function returns a string that will be displayed in the browser when the URL is accessed
+    return render_template('home.html', jobs=JOBS, company_name=' Raj ') # this function returns a string that will be displayed in the browser when the URL is accessed
 
 @app.route('/friend/')
 def hello_world2():
